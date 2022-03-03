930653f1d986923277cdbe5f488551423ef0cb29
rafael mejia <rafaelm@rafaels-MacBook-Pro.local>
2022-03-02 21:54:29 -0600

    todo-app-vutefify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Something about this page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, maiores? Molestias corrupti maiores obcaecati error ipsa nemo inventore, nam praesentium est beatae quae! Odio eligendi dignissimos doloremque. Culpa, odit voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, maiores? Molestias corrupti maiores obcaecati error ipsa nemo inventore, nam praesentium est beatae quae! Odio eligendi dignissimos doloremque. Culpa, odit voluptatem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, maiores? Molestias corrupti maiores obcaecati error ipsa nemo inventore, nam praesentium est beatae quae! Odio eligendi dignissimos doloremque. Culpa, odit voluptatem!</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-list
+      flat
+      class="pt-0"
+    >
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      hide-details
+      append-icon="mdi-plus"
+      clearable
+      color="teal lighten-1"
+    ></v-text-field>
+      <div
+        v-for='task in tasks'
+        :key='task.id'
+      >
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'teal lighten-4' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content
+            >
+              <v-list-item-title
+                 :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn 
+                icon
+                @click.stop="deleteTask(task.id)"
+              >
+                <v-icon color="green lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      newTaskTitle: '',
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Take a shower',
+          done: false
+        },
+        {
+          id: 3,
+          title: 'Make breakfast',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    addTask(){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
+    doneTask(id){
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id){
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
   }
 }
+
 </script>
